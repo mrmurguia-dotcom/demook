46f5d3d4a33ebb7f5aeea84fdf03f82eb7e121a9
mrmurguia-dotcom <mr_murguia@hotmail.com.ar>
2026-03-19 02:10:15 -0300

    Update sw.js

--- qrautoestiba/sw.js
+++ qrautoestiba/sw.js
@@ -1,10 +1,26 @@
-const CACHE_NAME = 'qr-logistica-v1';
-const assets = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'qr-autoestiba-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './android-icon-192x192.png',
+  './apple-icon-180x180.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// Instalar el Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Activar y responder (necesario para que sea instalable)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
